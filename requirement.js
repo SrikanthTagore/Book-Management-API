300eaea4d803abf972ae38da0c7388623432f05f
SrikanthTagore <93388691+SrikanthTagore@users.noreply.github.com>
2022-01-23 15:13:55 +0530

    Add files via upload

---
+++ requirement.js
@@ -0,0 +1,28 @@
+//Requirements for our projects
+
+//Book management company
+
+
+//ISBN,title,pub dare,language
+
+//AUTHORS
+//id,name,books[]
+
+//Publications
+//id,name,books[]
+
+//What are the APIs we need?
+
+//books
+//We need an API for
+//-> to get all books
+//-> to get specific books
+//-> to get a list of books based on category
+//-> to get a list of books based on languages
+
+//AUTHORS
+//->to get all AUTHORS
+//-> to get specific AUTHOR
+//-> to get a list of authors on books
+
+//Publications
